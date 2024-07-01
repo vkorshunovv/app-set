e64b66cf34b1aaf75d35b804fb47c8f37127520c
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-07-01 13:50:51 +0200

    add color changer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Accordian />
+      {/* <Accordian /> */}
+      <RandomColor />
     </div>
   );
 }
---
+++ src/components/random-color/index.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+const RandomColor = () => {
+  const [colorType, setColorType] = useState("hex");
+  const [color, setColor] = useState("#000000");
+
+  const randomColorUtility = (length) => {
+    return Math.floor(Math.random() * length);
+  };
+
+  const handleGenerateRandomHexColor = () => {
+    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[randomColorUtility(hex.length)];
+    }
+    setColor(hexColor);
+  };
+
+  const handleGenerateRandomRgbColor = () => {
+    const r = randomColorUtility(256);
+    const g = randomColorUtility(256);
+    const b = randomColorUtility(256);
+
+    setColor(`rgb(${r},${g},${b})`);
+  };
+
+  const containerStyle = {
+    width: "100vw",
+    height: "100vh",
+    background: color,
+    display: "flex",
+    justifyContent: "center",
+    gap: "1rem",
+  };
+
+  const buttonStyle = {
+    height: "4rem",
+    padding: "1rem",
+    borderRadius: "1rem",
+  };
+
+  return (
+    <div className="container" style={containerStyle}>
+      <button style={buttonStyle} onClick={() => setColorType("hex")}>
+        Create HEX button
+      </button>
+      <button style={buttonStyle} onClick={() => setColorType("rgb")}>
+        Create RGB color
+      </button>
+      <button
+        style={buttonStyle}
+        onClick={
+          colorType === "hex"
+            ? handleGenerateRandomHexColor
+            : handleGenerateRandomRgbColor
+        }
+      >
+        Generate Random Button
+      </button>
+    </div>
+  );
+};
+
+export default RandomColor;
